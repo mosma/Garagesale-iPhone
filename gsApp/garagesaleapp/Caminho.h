0f9978637c0328b03e839392fd8d650b0ddcdf13
Tarek Abdala <tarek@mosma.us>
2012-09-29 23:08:46 -0300

    updates at EntityClass, Settings Profile Update 100%

---
+++ gsApp/garagesaleapp/Caminho.h
@@ -0,0 +1,13 @@
+//
+//  Caminho.h
+//  garagesaleapp
+//
+//  Created by Tarek Jradi on 28/09/12.
+//  Copyright (c) 2012 MOSMA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Caminho : NSObject
+
+@end
---
+++ gsApp/garagesaleapp/Caminho.m
@@ -0,0 +1,13 @@
+//
+//  Caminho.m
+//  garagesaleapp
+//
+//  Created by Tarek Jradi on 28/09/12.
+//  Copyright (c) 2012 MOSMA. All rights reserved.
+//
+
+#import "Caminho.h"
+
+@implementation Caminho
+
+@end
